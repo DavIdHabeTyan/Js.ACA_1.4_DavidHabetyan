119a0da37608e7501b8b32a3de43eb88b419f47a
David Habetyan <davidhabetyanm@gmail.com>
2022-05-19 19:19:27 +0400

    added new branch and added folder homework5 exercises

---
+++ homework5/index.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+//exercises 3
+function delay(f, ms) {
+const sum = 5 + 5;
+setTimeout(function() {
+   f(sum)
+}, ms)
+}
+delay(function(callback){
+   console.log(callback)
+}, 3000)
+
+
+//exercises 4
+
+function debounce(f, ms) {
+
+   let isCooldown = false;
+
+   return function() {
+      if (isCooldown) return;
+
+      f.apply(this, arguments);
+
+      isCooldown = true;
+
+      setTimeout(() => isCooldown = false, ms);
+   };
+
+}
+
