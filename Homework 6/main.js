16c3589f3faad28d99212e67f82bc23e0a9fba32
David Habetyan <davidhabetyanm@gmail.com>
2022-05-28 22:53:19 +0400

    aded exercises homework 6

--- Homework 6/main.js
+++ Homework 6/main.js
@@ -25,13 +25,57 @@ let tree = {
 // console.log(creatArrayIsObj(tree))
 
 //2. Get array of nodes from tree.
+//
+// function creatArrayIsObjTree(obj, arr = []) {
+//    for (let elem in obj) {
+//       arr.push({id: elem, children: Object.keys(obj[elem])});
+//       creatArrayIsObjTree(obj[elem], arr)
+//    }
+//    return arr
+// }
+// console.log(creatArrayIsObjTree(tree))
 
-function creatArrayIsObjTree(obj, arr = []) {
-   for (let elem in obj) {
-      arr.push({id: elem, children: Object.keys(obj[elem])});
-      creatArrayIsObjTree(obj[elem], arr)
+
+
+// 3. Implement binary search.
+
+// let array = [12, 34, 5, 6, 78, 89, 3];
+// function binarySearch(numb, arr) {
+//    for(let i = 0; i < arr.length; i++) {
+//       if(numb === arr[i]){
+//          return `${true}, index: ${i}`
+//       }
+//    }
+//    return `${false}, not found`
+// }
+//
+// console.log(binarySearch(5, arr))
+
+//3. Implement binary search.
+
+   let array = [12, 34, 5, 6, 78, 89, 3];
+function binarySearch(arr, numb) {
+   let sortArr = arr.sort((a, b) => a-b);
+   let left = 0;
+   let right = sortArr.length - 1;
+   let mid;
+   while (left <= right){
+      mid = Math.round((right - left) / 2) + left;
+
+      if(numb === sortArr[mid]) {
+         return mid;
+      } else if(numb < sortArr[mid]) {
+         right = mid - 1;
+      } else {
+         left = mid + 1;
+      }
+
    }
-   return arr
+return false ;
 }
-console.log(creatArrayIsObjTree(tree))
+console.log(binarySearch(array, 3))
 
+
+// 4. Create a simple portfolio.
+
+// http://cvdavidhabetyandev.zzz.com.ua/
